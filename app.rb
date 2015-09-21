ab18c417892eb8ef05adf25da22782f7c35655eb
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2015-09-21 15:51:54 +0900

    Add app.rb

---
+++ app.rb
@@ -0,0 +1,35 @@
+ENV['RACK_ENV'] ||= 'development'
+require 'bundler/setup'
+Bundler.require(:default, ENV['RACK_ENV'])
+Dotenv.load if defined?(Dotenv)
+
+class App < Sinatra::Base
+  configure do
+    enable :sessions
+
+    set :assets_css_compressor, :sass
+    set :assets_js_compressor, :uglifier
+    set :assets_digest, false if settings.development?
+    register Sinatra::AssetPipeline
+    if defined?(RailsAssets)
+      RailsAssets.load_paths.each { |path| settings.sprockets.append_path(path) }
+    end
+  end
+
+  configure :production do
+    set :session_secret, ENV['SESSION_SECRET']
+    # use Rack::SSL
+  end
+
+  configure :development do
+    set :session_secret, 'b666fc9b4d7c33c95d73ca9c11c855b80650c9075cc3df53f08e9e9d3ad3ba43c4a580ad7ad45284159d499087563ff9577c84da09a38a1e8928ed79ca772d4c'
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+    use Rack::LiveReload
+    set :slim, pretty: true, sort_attrs: false
+  end
+
+  get '/' do
+    slim :index
+  end
+end
